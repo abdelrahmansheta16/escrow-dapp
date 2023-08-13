8339508fabc0fd1b80ff57c522cb841db04f01bc
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-08-13 17:49:36 +0300

    First Commit

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config()
+module.exports = {
+  solidity: "0.8.17",
+  paths: {
+    artifacts: "./app/src/artifacts",
+  },
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
